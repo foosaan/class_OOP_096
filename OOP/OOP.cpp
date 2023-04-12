d52dd658b4170fa8f2fdc4e01d36240bfc6d739c
foosaan <fauzan4297@gmail.com>
2023-04-12 10:54:43 +0700

    create void input

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -6,4 +6,13 @@ private:
 	int nim;
 	string nama;
 	float nilai;
-
+public:
+	void input() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama : ";
+		cin >> nama;
+		cout << "Masukkan Nilai : ";
+		cin >> nilai;
+	}
+	
